57b84ed4731917cffe85d592d3dd69661cb9989b
benfig1127 <benjifig@gmail.com>
2026-03-09 22:00:28 -0500

    handle invalid commands sol

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <string>
 
-int main() {
+namespace {
+  void prepare_shell() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
   std::cout << "$ ";
+  }
+
+  std::string read_input() {
+    std::string buffer;
+    std::getline(std::cin, buffer);
+    return buffer;
+  }
+
+  void print_input(const std::string& input) {
+    std::cout << input << ": command not found" << std::endl;
+  }
+}
+
+
+int main() {
+  prepare_shell();
+  print_input(read_input());
 }
